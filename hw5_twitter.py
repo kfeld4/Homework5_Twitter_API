4d06f55014a91cf0f81a9f4422473544c79f6c80
Kevin Feldman <kfeld@umich.edu>
2018-02-11 20:52:35 -0500

    caching/finding frequent words

---
+++ hw5_twitter.py
@@ -0,0 +1,104 @@
+from requests_oauthlib import OAuth1
+import json
+import sys
+import requests
+import secret_data # file that contains OAuth credentials
+import nltk # uncomment line after you install nltk
+
+## SI 206 - HW
+## COMMENT WITH:
+## Your section day/time: Section 003, Wednesday 9AM
+## Any names of people you worked with on this assignment:
+
+#usage should be python3 hw5_twitter.py <username> <num_tweets>
+username = sys.argv[1]
+num_tweets = sys.argv[2]
+
+consumer_key = secret_data.CONSUMER_KEY
+consumer_secret = secret_data.CONSUMER_SECRET
+access_token = secret_data.ACCESS_KEY
+access_secret = secret_data.ACCESS_SECRET
+
+#Code for OAuth starts
+url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
+requests.get(url, auth=auth)
+#Code for OAuth ends
+
+#Write your code below:
+#Code for Part 3:Caching
+#Finish parts 1 and 2 and then come back to this
+
+# on startup, try to load the cache from file
+CACHE_FNAME = 'twitter_cache.json'
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    CACHE_DICTION = json.loads(cache_contents)
+    cache_file.close()
+except:
+    CACHE_DICTION = {}
+
+def params_unique_combination(baseurl, params):
+    alphabetized_keys = sorted(params.keys())
+    res = []
+    for k in alphabetized_keys:
+        res.append("{}-{}".format(k, params[k]))
+    return baseurl + "_".join(res)
+
+def make_request_using_cache(baseurl, params):
+    unique_ident = params_unique_combination(baseurl,params)
+
+    ## first, look in the cache to see if we already have this data
+    if unique_ident in CACHE_DICTION:
+        print("Fetching cached data...")
+        return CACHE_DICTION[unique_ident]
+
+    ## if not, fetch the data afresh, add it to the cache,
+    ## then write the cache to file
+    else:
+        print("Making a request for new data...")
+        # Make the request and cache the new data
+        resp = requests.get(baseurl, params, auth=auth)
+        CACHE_DICTION[unique_ident] = json.loads(resp.text)
+        dumped_json_cache = json.dumps(CACHE_DICTION)
+        fw = open(CACHE_FNAME,"w")
+        fw.write(dumped_json_cache)
+        fw.close() # Close the open file
+        return CACHE_DICTION[unique_ident]
+
+#Code for Part 1:Get Tweets
+baseurl = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+
+params = {"screen_name" : "umsi", "count" : 25}
+#r = requests.get(protected_url, params=params, auth=auth).text
+#print (r.text)
+
+# twitter_dict = {}
+# lst_tweets=json.loads(r)
+# fw = open("tweet.json","w")
+# fw.write(json.dumps(twitter_dict))
+# fw.close()
+
+# print(json.dumps(twitter_dict, indent=2))
+
+#Code for Part 2:Analyze Tweets
+lst_tweets = make_request_using_cache(baseurl, params)
+tweets = []
+for tweet in lst_tweets:
+    tweets.append(tweet["text"])
+
+big_string = ''.join(tweets)
+tokens = nltk.word_tokenize(big_string)
+freqDist = nltk.FreqDist(token for token in tokens if token.isalpha() and "www." not in token)
+for word, frequency in freqDist.most_common(5):
+    print(word + " " + str(frequency))
+
+
+if __name__ == "__main__":
+    if not consumer_key or not consumer_secret:
+        print("You need to fill in client_key and client_secret in the secret_data.py file.")
+        exit()
+    if not access_token or not access_secret:
+        print("You need to fill in this API's specific OAuth URLs in this file.")
+        exit()
